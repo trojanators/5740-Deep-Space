eb66066ecfdef041733c1409b6c3abe477b6e228
trojanators <frc5740@gmail.com>
2019-02-19 15:39:06 -0800

    ssss

--- 2019-Robot/Robot/src/main/java/frc/robot/logging/SuperShuffle.java
+++ 2019-Robot/Robot/src/main/java/frc/robot/logging/SuperShuffle.java
@@ -1,80 +1,56 @@
 package frc.robot.logging;
 
 import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.ComplexWidget;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.shuffleboard.SimpleWidget;
 import frc.robot.*;
 import frc.robot.subsystems.Drivetrain;
 
 public class SuperShuffle {
-
+    public static  String cam = "mjpeg:http://" + "10.57.40.105:1182/?action=stream";
     private static ShuffleboardTab tab = Shuffleboard.getTab("Driver Info");
     private static NetworkTableEntry controller1XEntry = tab.add("controller1 X value", OI.controller1.getRawAxis(1))
-            .withWidget(BuiltInWidgets.kNumberBar)
-            .withPosition(0, 0)
-            .withSize(2, 1)
-            .getEntry();
+            .withWidget(BuiltInWidgets.kNumberBar).withPosition(0, 0).withSize(2, 1).getEntry();
 
-    private static NetworkTableEntry controller1YEntry = 
-    tab.add("Controller 1 Y value", OI.controller1.getRawAxis(4))
-            .withWidget(BuiltInWidgets.kNumberBar)
-            .withPosition(0, 1)
-            .withSize(2, 1)
-            .getEntry();
+    private static NetworkTableEntry controller1YEntry = tab.add("Controller 1 Y value", OI.controller1.getRawAxis(4))
+            .withWidget(BuiltInWidgets.kNumberBar).withPosition(0, 1).withSize(2, 1).getEntry();
 
-    private static ComplexWidget pdp = 
-    tab.add("Power",OI.pdp)
-    .withWidget(BuiltInWidgets.kPowerDistributionPanel)
-    .withPosition(0, 2)
-    .withSize(3, 3);
-/*
-    private static NetworkTableEntry pressureEntry =
-    tab.add("pressure", OI.pressureSwitch.getPressureSwitchValue() )
-   .withPosition(9,2)
-   .withSize(2, 1)
-   .withWidget(BuiltInWidgets.kBooleanBox)
-   .getEntry();*/
+    private static ComplexWidget pdp = tab.add("Power", OI.pdp).withWidget(BuiltInWidgets.kPowerDistributionPanel)
+            .withPosition(0, 2).withSize(3, 3);
+    /*
+     * private static NetworkTableEntry pressureEntry = tab.add("pressure",
+     * OI.pressureSwitch.getPressureSwitchValue() ) .withPosition(9,2) .withSize(2,
+     * 1) .withWidget(BuiltInWidgets.kBooleanBox) .getEntry();
+     */
 
-   private static NetworkTableEntry speedEntry = 
-    tab.add("axis acc",OI.accelerometer.getX())
-    .withPosition(8,3)
-    .withSize(3, 2)
-    .withWidget(BuiltInWidgets.kGraph)
-    .getEntry();
-    
-    private static NetworkTableEntry ShiftEntry = 
-    tab.add ("Shifting", Drivetrain.state)
-    .withPosition(9,2)
-    .withSize(2,1)
-    .withWidget(BuiltInWidgets.kBooleanBox)
-    .getEntry();
+    private static NetworkTableEntry speedEntry = tab.add("axis acc", OI.accelerometer.getX()).withPosition(8, 3)
+            .withSize(3, 2).withWidget(BuiltInWidgets.kGraph).getEntry();
 
-    private static NetworkTableEntry pressureEntry= 
-    tab.add ("CompressorStatus", OI.pressureSwitch.getPressureSwitchValue())
-    .withPosition(9,1)
-    .withSize(2,1)
-    .withWidget(BuiltInWidgets.kBooleanBox)
-    .getEntry();
-    
-     private static NetworkTableEntry balllimitEnrty = 
-     tab.add("Claw status", OI.ballLimit.get())
-     .withPosition(9,0)
-     .withSize(2,1)
-     .withWidget(BuiltInWidgets.kBooleanBox)
-     .getEntry();
-     
-     private static NetworkTableEntry cameraETableEntry=
-     tab.add("LiveFeed","CamraPublisher/cam1/mjpg:http://10.57.40.105:1181/?action=stream")
-     .withPosition(3,6)
-     .withSize(4,5)
+    private static NetworkTableEntry ShiftEntry = tab.add("Shifting", Drivetrain.state).withPosition(9, 2)
+            .withSize(2, 1).withWidget(BuiltInWidgets.kBooleanBox).getEntry();
+
+    private static NetworkTableEntry pressureEntry = tab
+            .add("CompressorStatus", OI.pressureSwitch.getPressureSwitchValue()).withPosition(9, 1).withSize(2, 1)
+            .withWidget(BuiltInWidgets.kBooleanBox).getEntry();
+
+    private static NetworkTableEntry balllimitEnrty = tab.add("Claw status", OI.ballLimit.get()).withPosition(9, 0)
+            .withSize(2, 1).withWidget(BuiltInWidgets.kBooleanBox).getEntry();
+
+    /*private static NetworkTableEntry cameraEntry =
+     tab.add("LiveFeed",cam)
+     .withPosition(4,3)
+     .withSize(4,3)
      .withWidget(BuiltInWidgets.kCameraStream)
-.getEntry();
+     .getEntry();
+     */
      public static void Periodic() {
-        cameraETableEntry.setString("CamraPublisher/cam1/mjpg:http://10.57.40.105:1181/?action=stream");
+
+    
        balllimitEnrty.setBoolean(OI.ballLimit.get());
-      
         ShiftEntry.setBoolean(Drivetrain.state);
         speedEntry.setDouble(OI.accelerometer.getY());
         speedEntry.setDouble(OI.accelerometer.getX());
