300b6e2641db24c70f517eb55939b0aa1663acc8
nicholasblackburn1 <nickblackburn02@gmail.com>
2019-04-02 06:31:43 -0400

    added open cv build to install script

---
+++ Vision/GRIP2.0/src/main/java/Grip2/run.py
@@ -0,0 +1,18 @@
+import grip 
+
+class main:
+   
+
+    grip.grip.__init__
+
+
+    grip.grip.process.source0 = ("http://frcvision.local:1181.mjpeg")
+    grip.grip.process.__globals__.
+
+    grip.grip.process
+
+
+
+
+
+ 
